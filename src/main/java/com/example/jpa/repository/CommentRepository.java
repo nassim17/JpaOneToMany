af9d2ead86e74fdf414b160dca38cfa0c39a975c
Nassim Abed <nabed@sodifrance.fr>
2018-10-30 18:00:00 +0100

    Create PostRepository and CommentRepository

--- src/main/java/com/example/jpa/repository/CommentRepository.java
+++ src/main/java/com/example/jpa/repository/CommentRepository.java
@@ -1,7 +1,8 @@
 package com.example.jpa.repository;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
